1e5da62dbcf370e5385df1aa2be2f3815b0d2de8
Alfonso Gonzalez <info@wsemantic.com>
2024-04-03 12:54:49 +0200

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -20,7 +20,7 @@
     'version': '16.0.0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['sales'], 
+    'depends': ['sale'], 
 
     # always loaded
     'data': [
